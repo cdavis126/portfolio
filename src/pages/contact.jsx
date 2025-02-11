5ea1b2aedde1f0915c4d1d856c45c7d7adad2405
Cherie Davis <cheriedavis126@gmail.com>
2025-02-11 18:36:09 -0500

    updating contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBContainer,
+  MDBRow,
+  MDBCol
+} from 'mdb-react-ui-kit';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import sunsetImage from '../assets/beautysunset.jpg'; // Import your custom image
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>  {/* ✅ Update this to match the page */}
-      <p>This page is under construction.</p>
-    </div>
+    <MDBContainer fluid className="d-flex justify-content-center align-items-center vh-100 bg-black">
+      <MDBRow className="w-100 justify-content-center">
+        <MDBCol md="8" lg="6">
+          <MDBCard className="text-center shadow-lg border-0">
+            <MDBCardImage position="top" src={sunsetImage} alt="Connect" />
+            <MDBCardBody className="bg-dark text-white">
+              <MDBCardTitle className="fw-bold">
+                <a href="mailto:cheriedavis126@gmail.com" className="text-gradient text-decoration-none">
+                  Connect with Me
+                </a>
+              </MDBCardTitle>
+              <MDBCardText>
+                "The only way to do great work is to love what you do." - Steve Jobs
+              </MDBCardText>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
 };
 
